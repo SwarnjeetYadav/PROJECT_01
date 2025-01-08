abd13aa1f9823c60e7cfbe41097487a1d05815f6
Swarnjeet yadav <111694586+SwarnjeetYadav@users.noreply.github.com>
2025-01-09 01:31:08 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb+srv://swarnjeet9155:Swarn123@swarnjeet.f9vjx0z.mongodb.net/Lux?retryWrites=true&w=majority&appName=Swarnjeet";    
+const MONGO_URL = "mongodb+srv://<username>:<password>@swarnjeet.f9vjx0z.mongodb.net/Lux?retryWrites=true&w=majority&appName=Swarnjeet";    
 
 main()
   .then(() => {
